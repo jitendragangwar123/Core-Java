004086922abce62ff3b3889037e8fe5d40a68574
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2021-04-07 01:18:08 +0530

    Update Calcu.java

--- Calcu.java
+++ Calcu.java
@@ -3,6 +3,19 @@ import java.util.*;
 public class Calcu {
 	double i;
 	double x=Math.sqrt(i);
-	
+}
+
+//Main.java
+package calc;
+import java.lang.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calcu a=new Calcu();
+		 a.i=16.0f;
+		System.out.println(a.x);
+
+	}
 
 }
