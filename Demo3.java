0673302fcb41034d14a3e1865e686a65c2bda37e
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2021-04-07 01:23:20 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,13 @@
+package maccess;
+import java.util.*;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    try(Scanner sc=new Scanner(System.in);){
+    	System.out.println("");
+    }
+	}
+
+}
